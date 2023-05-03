e4fa36bf22b369c765575f213fdd6dbb34c365aa
rknm-cell <racknahm@gmail.com>
2023-05-02 20:43:09 -0400

    completed tests

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size = 0, ): 
+        self.brand = brand
+        self.size = size
+       
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")   
+    def set_size(self):
+        return self._size 
+    def get_size(self, value):
+        if type(value) == int:
+            self._size = value
+        else:
+            print("size must be an integer")
+    size = property(set_size, get_size)
+    
